235013867c54a535dd03f16d16f046d03fcf3287
amirhefny <hefnamir48@gmail.com>
2025-08-26 05:32:57 +0300

    edit addTextContentInput locator

--- pages/CoursePartsPage.ts
+++ pages/CoursePartsPage.ts
@@ -12,7 +12,7 @@ export class CoursePartsPage extends BasePage {
 
     private addTextCourse = () => this.page.locator("//div[@class='grid w-full grid-cols-3 gap-3']//a[5]");
     private addTextTitleInput = () => this.page.locator("//input[@name='title']");
-    private addTextContentInput = () => this.page.locator("(//div[@class='tiptap ProseMirror focus:outline-none p-5 h-full undefined'])[1]");
+    private addTextContentInput = () => this.page.locator("//*[@id='__next']/div[1]/main/div[3]/div/form/div/div[1]/div[2]/div/div/div/div[2]/div");
     private addNewTextCourseSubmitButton = () => this.page.locator("//div[@id='sidebar']//button[@type='submit']");
     private selectNewItem = () => this.page.locator("//h3[@data-state='closed']");
     private selectExam = () => this.page.locator("//div[@ class ='flex justify-between']//button");
